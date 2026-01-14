75243d03426225e2e90dffecd9a6c331fa6e763d
Krishna Aryal <info.karyal@gmail.com>
2026-01-14 14:16:58 +0545

    2:16 Jan 14 2026

---
+++ GroupE_CoreProgramming/src/day2/MyCalculator.java
@@ -0,0 +1,26 @@
+package day2;
+
+import java.util.Scanner;
+
+public class MyCalculator {
+	//Enter first number : 9
+	//Enter second number : 4
+	//Sum: 13
+	
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum of the two numbers = " + sum);
+
+        sc.close();
+    }
+}
