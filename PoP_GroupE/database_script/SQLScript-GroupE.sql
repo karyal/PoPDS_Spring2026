8b85042247edebac2719ee717a7ef4e1bff4f9e9
Krishna Aryal <info.karyal@gmail.com>
2026-02-22 11:34:15 +0545

    11:34 22 Feb 2026

--- PoP_GroupE/database_script/SQLScript-GroupE.sql
+++ PoP_GroupE/database_script/SQLScript-GroupE.sql
@@ -3,3 +3,30 @@
 
 create database GroupE;
 use GroupE;
+
+create table tbl_users(
+	uid int not null,
+    user_name varchar(50) not null,
+    user_password varchar(50) not null
+);
+
+alter table tbl_users
+	add constraint pk_uid
+		primary key(uid);
+
+alter table tbl_users
+	add constraint uk_user_name
+		unique key (user_name);
+
+describe tbl_users;
+
+-- Insert record
+insert into tbl_users values(1, 'admin','admin');
+insert into tbl_users values(2, 'root','root');
+insert into tbl_users values(3, 'user1', 'user1');
+
+-- Select records
+select * from tbl_users;
+
+-- delete record
+delete from tbl_users where uid=3;
---
+++ PoP_GroupE/src/day21/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package day21;
+
+public class DatabaseConnection {
+
+	public static void main(String[] args) {
+		//Create a project (JavaFX Project) - ok
+		//Convert to Maven project - pom.xml
+		//Add mysql connector library - https://mvnrepository.com/
+		//Connect with database - Java Code
+		
+		String DRIVER="";
+		String HOST="";
+		int PORT=0;
+		String USER="";
+		String PASS="";
+		String DBNAME="";
+		String URL="";
+
+	}
+
+}
